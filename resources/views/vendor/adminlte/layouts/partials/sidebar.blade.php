eaed46d570193e5b1eaf38ad6d4a503ca17a5ed2
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-06-13 21:24:22 +0000

    Removing code using Gravatar and other parts using Auth.

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -4,20 +4,6 @@
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
 
-        <!-- Sidebar user panel (optional) -->
-        @if (! Auth::guest())
-            <div class="user-panel">
-                <div class="pull-left image">
-                    <img src="{{ Gravatar::get($user->email) }}" class="img-circle" alt="User Image" />
-                </div>
-                <div class="pull-left info">
-                    <p>{{ Auth::user()->name }}</p>
-                    <!-- Status -->
-                    <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('adminlte_lang::message.online') }}</a>
-                </div>
-            </div>
-        @endif
-
         <!-- search form (Optional) -->
         <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
