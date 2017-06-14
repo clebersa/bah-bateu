8f82793160e0d85905919e424d52a66a6a7c3c05
Cleber de Souza Alcântara <cleber.93cd@gmail.com>
2017-06-14 16:08:52 -0300

    Adding first files and changes to add timeseries chart.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,30 +6,5 @@
 
 
 @section('main-content')
-	<div class="container-fluid spark-screen">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-
-				<!-- Default box -->
-				<div class="box">
-					<div class="box-header with-border">
-						<h3 class="box-title">Home</h3>
-
-						<div class="box-tools pull-right">
-							<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-								<i class="fa fa-minus"></i></button>
-							<button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-								<i class="fa fa-times"></i></button>
-						</div>
-					</div>
-					<div class="box-body">
-						{{ trans('adminlte_lang::message.logged') }}. Start creating your amazing application!
-					</div>
-					<!-- /.box-body -->
-				</div>
-				<!-- /.box -->
-
-			</div>
-		</div>
-	</div>
+<svg width="960" height="500"></svg>
 @endsection
