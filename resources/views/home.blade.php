93e1015d096c2aa3d83919d9d55bbac286ca3a76
Cleber de Souza Alcântara <cleber.93cd@gmail.com>
2017-07-30 18:24:00 -0300

    Removing borders in the home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,22 +7,22 @@
 
 @section('main-content')
 <div class="row">
-    <div class="col-lg-12 overviewContainer" style="border-style: solid;">
+    <div class="col-lg-12 overviewContainer">
         <div class="text-center overview"></div>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-6 col-md-6">
         <div class="row">
-            <div class="col-lg-12 heatmapContainer no-padding" style="border-style: solid;">
+            <div class="col-lg-12 heatmapContainer no-padding">
                 <div class="heatmap col-lg-12"></div>
             </div>
-            <div class="col-lg-12 scatterContainer no-padding" style="border-style: solid;">
+            <div class="col-lg-12 scatterContainer no-padding">
                 <div class="scatter col-lg-12"></div>
             </div>
         </div>
     </div>
-    <div class="col-lg-6 col-md-6" style="border-style: solid;">
+    <div class="col-lg-6 col-md-6">
         <div class="floating-panel btn-group text-center">
             <button id="pointsButton" type="button" class="btn btn-sm btn-default" style="background-color: white; font-size: 11px;">Points</button>
             <button id="heatMapButton" type="button" class="btn btn-sm btn-default" style="background-color: white; font-size: 11px;">Heatmap</button>
