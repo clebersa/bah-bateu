b0f4c932a7aaf44fb7dcb802c8b1b8627094acf5
Cleber de Souza Alcântara <cleber.93cd@gmail.com>
2018-09-06 17:59:32 -0300

    Adding icons to panels.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <div class="col-lg-12 overviewContainer">
         <div class="box box-success box-solid">
             <div class="box-header">
-                <h3 class="box-title">Time and People Distribution</h3>
+                <h3 class="box-title"><i class="fa fa-calendar"></i> Time and People Distribution</h3>
                 <div class="box-tools pull-right">
                 </div>
             </div>
@@ -29,7 +29,7 @@
             <div class="col-lg-12 heatmapContainer">
                 <div class="box box-danger box-solid">
                     <div class="box-header">
-                        <h3 class="box-title">Daily Heatmap</h3>
+                        <h3 class="box-title"><i class="fa fa-thermometer"></i> Daily Heatmap</h3>
                     </div>
                     <div class="box-body no-padding">
                         <div class="heatmap col-lg-12"></div>
@@ -43,7 +43,7 @@
             <div class="col-lg-12 scatterContainer">
                 <div class="box box-primary box-solid">
                     <div class="box-header">
-                        <h3 class="box-title">Vehicle Distribution</h3>
+                        <h3 class="box-title"><i class="fa fa-car"></i> Vehicle Distribution</h3>
                         <div class="box-tools pull-right">
                             <span class="label bg-red hidden" id="errorLabelScatter">
                                 <i class="fa fa-warning"></i> Unable to load data.
@@ -65,7 +65,7 @@
     <div class="col-lg-6 col-md-6">
         <div class="box box-success box-solid">
             <div class="box-header">
-                <h3 class="box-title">Geographic Distribution</h3>
+                <h3 class="box-title"><i class="fa fa-map"></i> Geographic Distribution</h3>
                 <div class="box-tools pull-right">
                     <span class="label bg-red hidden" id="errorLabelMaps">
                         <i class="fa fa-warning"></i> Unable to load data.
